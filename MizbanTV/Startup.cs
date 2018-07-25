4028d76b1e7f3e90794ad59d92f461dc911f4f88
Abbas.d67 <Abbas.d67@gmail.com>
2018-07-25 13:43:09 +0430

    init

---
+++ MizbanTV/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MizbanTV.Startup))]
+namespace MizbanTV
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
